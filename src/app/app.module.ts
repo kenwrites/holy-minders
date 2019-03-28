fbcd92e9c389525c9ff58666aacae546fac22481
kenwrites <kenwrites@users.noreply.github.com>
2019-03-28 11:25:36 -0400

    Adding main footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainHeaderComponent
+    MainHeaderComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule
